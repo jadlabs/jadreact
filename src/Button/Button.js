f74ddba1211de38940fdcbf4f2ddaa6262a9623f
Jimmy Murillo <salchichongallo1997@gmail.com>
2020-07-27 18:00:57 -0500

    feat(Button): add primary styles

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import { darken, lighten } from 'polished';
 import styled from 'styled-components';
 
 export const Button = styled.button`
-  color: red;
+  background: none;
+  border: none;
+  padding: 0;
+  outline: none;
+
+  padding: 0.75rem 1rem;
+  border-radius: 0.25rem;
+  font-size: 18px;
+  font-weight: bold;
+  color: #fff;
+  background: #2196f3;
+  cursor: pointer;
+
+  &:hover {
+    background: ${lighten(0.08, '#2196f3')};
+  }
+
+  &:active {
+    background: ${darken(0.08, '#2196f3')};
+  }
 `;
